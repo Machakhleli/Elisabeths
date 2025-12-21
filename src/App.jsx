69f9ae7af3533405a4eb3d117d77f79b0212eba6
Machakhleli <ushangikakhidze@gmail.com>
2025-12-21 12:20:28 +0400

    fix: fix routing bugs afrer merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
-<<<<<<< HEAD
-// import { useState } from "react";
-// import Header from "./components/header/Header";
-// import MainPage from "./components/Main-Page";
-// import Footer from "./components/Footer";
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <MainPage />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-
-import Portfolio from "./pages/Portfolio/Portfolio";
-import Portraits from "./pages/Portfolio/Portraits";
-import Family from "./pages/Portfolio/Family";
-import Maternity from "./pages/Portfolio/Maternity";
-import Commercial from "./pages/Portfolio/Commercial";
-=======
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import MainPage from "./components/Main-Page";
 import Footer from "./components/Footer";
 import Services from "./pages/Services";
->>>>>>> 63510d7c254c9e2f7f90bb28f44eb3bec3d484b7
 
 function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
-
-      <Routes>
-        <Route path="/portfolio" element={<Portfolio />}>
-          <Route path="portraits" element={<Portraits />} />
-          <Route path="family" element={<Family />} />
-          <Route path="maternity" element={<Maternity />} />
-          <Route path="commercial" element={<Commercial />} />
-        </Route>
-      </Routes>
-=======
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/services" element={<Services />} />
@@ -60,7 +21,6 @@ function App() {
         <Route path="/portfolio/commercial" element={<h2>Commercial </h2>} />
       </Routes>
       <Footer />
->>>>>>> 63510d7c254c9e2f7f90bb28f44eb3bec3d484b7
     </>
   );
 }
