300e67846e66bffb81343fdc57b441037f03a22b
Machakhleli <ushangikakhidze@gmail.com>
2026-01-20 11:12:36 +0400

    fix: fixed footer possiotion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import { Instagram, Facebook, Globe } from "lucide-react"; // Using Lucide for a
 export default function Footer() {
   return (
     <footer className="w-full bg-black text-white px-6 md:px-12 py-12 border-t border-zinc-900">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
-        {/* Copyright Text */}
+      <div className="max-w-100% mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
         <div className="text-center md:text-left order-2 md:order-1">
           <span className="text-[10px] md:text-[11px] uppercase tracking-[0.2em] text-zinc-500 font-light">
             © {new Date().getFullYear()} Elisabeth GogoliSvili{" "}
